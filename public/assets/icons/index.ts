4cf3f7bfe66578627afddd052e8725e659d24520
pinktopaz <heather128@ewhain.net>
2023-05-18 01:19:05 +0900

    style : 출력뷰 사이드바 UI 구현

--- public/assets/icons/index.ts
+++ public/assets/icons/index.ts
@@ -1,1 +1,5 @@
 export { default as IcLogo } from './Ic_logo.svg';
+export { default as IcMore } from './Ic_more.svg';
+export { default as IcAddMiddle } from './Ic_addMiddle.svg';
+export { default as IcAddMain } from './Ic_addMain.svg';
+export { default as IcTrash } from './Ic_trash.svg';
