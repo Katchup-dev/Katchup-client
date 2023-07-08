396d2476e6fe37c7e6aad4dbbd5ec60b6c8179fb
parkhyeonji <iamphj3@naver.com>
2023-07-08 14:34:18 -0700

    feat : 대분류, 중분류, 소분류 조회(get) 커스텀 훅 생성

--- lib/hooks/useGetIndex.ts
+++ lib/hooks/useGetIndex.ts
@@ -1,7 +1,12 @@
-import { getCategories } from 'core/apis/input';
-import { InputCategoryInfo, KatchupResponse } from 'types/input';
+import { getCategories, getFolders, getTasks } from "core/apis/input";
+import {
+  InputCategoryInfo,
+  InputFolderInfo,
+  InputTaskInfo,
+  KatchupResponse
+} from "types/input";
 
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 
 export const useGetCategories = () => {
   const { data, isLoading, error } = useQuery<KatchupResponse<InputCategoryInfo[]>>(['categories'], getCategories);
@@ -12,3 +17,25 @@ export const useGetCategories = () => {
     isError: error,
   };
 };
+
+export const useGetFolders = () => {
+  const { data, isLoading, error } = useQuery<KatchupResponse<InputFolderInfo[]>>(['folders'], getFolders);
+
+  return {
+    folders: data?.data,
+    isLoading: isLoading,
+    isError: error,
+  };
+};
+
+
+export const useGetTasks = () => {
+  const { data, isLoading, error } = useQuery<KatchupResponse<InputTaskInfo[]>>(['tasks'], getTasks);
+
+  return {
+    tasks: data?.data,
+    isLoading: isLoading,
+    isError: error,
+  };
+};
+
