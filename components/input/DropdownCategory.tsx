f615acd714aa1d01f7e1e811a20f08c403684e29
parkhyeonji <iamphj3@naver.com>
2023-07-08 17:20:15 -0700

    feat : 대분류 추가 커스텀 훅 사용

--- components/input/DropdownCategory.tsx
+++ components/input/DropdownCategory.tsx
@@ -6,6 +6,8 @@ import { InputCategoryInfo } from 'types/input';
 
 import styled from '@emotion/styled';
 
+import { usePostCategory } from '../../lib/hooks/usePostIndex';
+
 interface dropdownIndexProps {
   options: InputCategoryInfo[];
   inputValue: string;
@@ -13,13 +15,15 @@ interface dropdownIndexProps {
 
 const DropdownCategory = ({ options, inputValue }: dropdownIndexProps) => {
   const [categorySelect, setCategorySelect] = useRecoilState(categorySelectState);
+  const postCategory = usePostCategory();
 
   const handleOptionClick = (option: InputCategoryInfo) => {
     setCategorySelect(option);
   };
 
   const handleAddIndex = (name: string) => {
-    postCategories(name);
+    const categoryData = { name: inputValue };
+    postCategory.createCategory(categoryData);
   };
 
   const displayOptions = () => {
