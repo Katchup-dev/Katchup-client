87b3ab14009a282b7db5befdabcddde78937e781
Dev-jwJeong <rgn95652@gmail.com>
2023-08-17 00:09:48 +0900

    feat : 키워드 추가 시 랜덤 배경색,글자색 기능 구현

---
+++ components/input/DropdownKeyword.tsx
@@ -0,0 +1,70 @@
+import styled from '@emotion/styled';
+
+interface KeyworkProps {
+  background: string;
+  color: string;
+}
+
+interface DropdownKeywordProps {
+  inputValue: string;
+  keywordColor: KeyworkProps;
+}
+
+const DropdownKeyword = ({ inputValue, keywordColor }: DropdownKeywordProps) => {
+  return (
+    <StDropdown>
+      <li>{inputValue && <DropdownKeyworkText keywordColor={keywordColor}>{inputValue}</DropdownKeyworkText>}</li>
+    </StDropdown>
+  );
+};
+
+export default DropdownKeyword;
+
+const StDropdown = styled.ul`
+  position: absolute;
+  top: 7.2rem;
+
+  z-index: 1;
+
+  width: 100%;
+  height: 28rem;
+
+  overflow: scroll;
+  padding: 1.2rem 1.4rem;
+
+  border: 0.1rem solid #e2e2e2;
+  border-radius: 0 0 0.8rem 0.8rem;
+  box-shadow: 0 0.2rem 0.4rem rgba(0, 0, 0, 0.23);
+  background-color: ${({ theme }) => theme.colors.katchup_white};
+
+  & > li {
+    display: flex;
+    justify-content: space-between;
+
+    margin-bottom: 1rem;
+
+    ${({ theme }) => theme.fonts.h2_smalltitle};
+
+    cursor: pointer;
+
+    & > svg {
+      display: block;
+
+      margin-bottom: -1rem;
+    }
+  }
+`;
+
+const DropdownKeyworkText = styled.div<{ keywordColor: KeyworkProps }>`
+  padding: 0.5rem 1rem;
+
+  border-radius: 0.8rem;
+  background-color: ${({ keywordColor }) => keywordColor.background};
+  color: ${({ keywordColor }) => keywordColor.color};
+
+  font-family: Pretendard;
+  font-size: 1.3rem;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+`;
