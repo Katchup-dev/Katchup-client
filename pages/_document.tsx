314cc76a1c9fa25ddbec837760cbd116be9986ce
parkhyeonji <iamphj3@naver.com>
2023-08-24 11:37:46 +0900

    chore : 파비콘 및 title 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
 //ssr이라 처음에는 스타일이 아무것도 안된 html만 보이니까 미리 초기 스타일을 지정해주는 파일!
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <title>Katchup</title>
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
       {/* 여기에 폰트 임포팅 // 여기에 공통 CSS e.g.reset-css/common.css */}
       <body>
