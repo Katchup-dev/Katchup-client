c0745cdda4e272d17b9282d7876a18c2cab1678c
parkhyeonji <iamphj3@naver.com>
2023-09-21 12:26:26 +0900

    chore : 불필요한 코드 제거

--- core/apis/auth.ts
+++ core/apis/auth.ts
@@ -3,16 +3,6 @@ import { client } from 'lib/axios';
 export const signup = async (accessToken: string) => {
   try {
     const { data } = await client.post('/auth', { accessToken });
-    console.log(data.data);
-    return data.data;
-  } catch (error) {
-    console.error(error);
-  }
-};
-
-export const getRefreshToken = async (accessToken: string, refreshToken: string) => {
-  try {
-    const { data } = await client.post('/auth/token', { accessToken, refreshToken });
     return data.data;
   } catch (error) {
     console.error(error);
